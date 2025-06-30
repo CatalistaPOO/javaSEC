4ccfc87c22dcf4c4aad3dab83343d7a4b027e302
CatalistaPOO <fgtpoo78@gmail.com>
2025-06-30 17:20:31 +0200

    ok

--- WS/J02_introduccion2/src/ejercicios/ejercicios06/Ej06.java
+++ WS/J02_introduccion2/src/ejercicios/ejercicios06/Ej06.java
@@ -6,7 +6,10 @@ public class Ej06 {
 		
 	}
 	
-	public static long potencia (int num) {
-		
+	public static long potencia (int num, int exponent) {
+		if (exponent == 0){
+			return Math.pow(num, exponent = 1);
+		}
+		return Math.pow(num, exponent);
 	}
 }
