4008e16bd6712b8d8cc3be52d36284442c1eb31e
CatalistaPOO <fgtpoo78@gmail.com>
2025-06-30 13:49:51 +0200

    30 junio

--- WS/J02_introduccion2/src/ejercicios/ejercicios06/Ej04.java
+++ WS/J02_introduccion2/src/ejercicios/ejercicios06/Ej04.java
@@ -3,19 +3,20 @@ package ejercicios.ejercicios06;
 public class Ej04 {
 	public static void main(String[] args) {
 //		Suma de los dígitos de un número natural.
-		int n = 123;
-		String cantidadDigitos = String.valueOf(n);
-		System.out.println(cantidadDigitos.charAt(Character.getNumericValue(n)));
-//		System.out.println(sumaDigitos(120));
+		int n = 1234;
+		
+		System.out.println(sumaDigitos(n));
 	}
 	
-//	public static int sumaDigitos(int num) {
-//		String cantidadDigitos = String.valueOf(num);
-//		if (num / 10 < 1) {
-//			System.out.println(Integer.valueOf(cantidadDigitos.charAt(num)));
-//			return Integer.valueOf(cantidadDigitos.charAt(num));  
-//		}
-//		System.out.println(Integer.valueOf(cantidadDigitos.charAt(num) + sumaDigitos(num / 10)));
-//		return Integer.valueOf(cantidadDigitos.charAt(num) + sumaDigitos(num / 10));
-//	}
+	public static int sumaDigitos(int num) {
+		String cantidadDigitos = String.valueOf(num);
+		int[] digitos = new int[cantidadDigitos.length()];
+		int index = 0;
+		if (num / 10 < 1) {
+			return digitos[0];
+		}
+//		return num / (int)Math.pow(10, cantidadDigitos.length() - index--);//1234
+		return num / 10 + sumaDigitos(num / cantidadDigitos.length()-1);//1234
+	}
 }
+//1234 /quedarse con 1 si divides entre la longitud (length)que no abarca la ultima cifra
