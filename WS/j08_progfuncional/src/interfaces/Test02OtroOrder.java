06bb52b4818e48f8d1f561ad28506bb46a0b6bc0
CatalistaPOO <fgtpoo78@gmail.com>
2025-07-08 13:52:42 +0200

    8JulioFin

--- WS/j08_progfuncional/src/interfaces/Test02OtroOrder.java
+++ WS/j08_progfuncional/src/interfaces/Test02OtroOrder.java
@@ -6,14 +6,29 @@ import java.util.TreeSet;
 
 public class Test02OtroOrder {
 	public static void main(String[] args) {
-		//creamos TreeSet estableciendo su orden por Comparator(definido por longitud de mayor a menor)
-		Set<String> set= new TreeSet<String>(new Comparator<String>() {
-			//Definimos compare que es el metodo abstracto
-			@Override
-			public int compare(String o1, String o2) {
-				return o2.length() - o1.length();
-			}
-		});
+		//SE DESCRIBE EL PASO DE CREAR UN TREESET POR 3 METODOS:
+		
+		
+		//1-creamos TreeSet estableciendo su orden por Comparator(definido por longitud de mayor a menor)
+//		Set<String> set= new TreeSet<String>(new Comparator<String>() {
+//			//Definimos compare que es el metodo abstracto
+//			@Override
+//			public int compare(String o1, String o2) {
+//				return o2.length() - o1.length();
+//			}
+//		});
+		
+//		//2-Lambda para crear TreeSet ordenado por método del cuerpo
+//		Set<String> set = new TreeSet<String>((String o1, String o2)->{
+//			return o1.length() - o2.length();
+//		});
+		
+		
+		//3-Lambda para crear TreeSet reducido al ser una u´nica innstruccion que recibe solo Strings
+		//el cuerpo es solo una línea fuera llaves y si devuelve algo asume que es un return(fuera return y ; del final de instrucción
+		Set<String> set = new TreeSet<String>((o1, o2)-> o1.length() - o2.length());
+		
+		
 		//Llenamos TreeSet, que respetará el orden indicado al crearlo, NO el natural.		
 		set.add("uno");
 		set.add("dos");
@@ -25,9 +40,15 @@ public class Test02OtroOrder {
 		set.add("Ávila");
 		set.add("ágil");
 		
-		for  (String s : set) {
-			System.out.println(s);
-		}
+//		for  (String s : set) {
+//			System.out.println(s);
+//		}
 		
+		//Diferentes representaciones de lambda, resumiendo cada vez más la expresión lambda
+		set.forEach((s)-> System.out.println(s));
+		
+		set.forEach(s -> System.out.println(s));
+
+		set.forEach(System.out::println);
 	}
 }
