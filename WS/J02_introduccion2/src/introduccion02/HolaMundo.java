e9714f8b1c9f282a7efd6e9e761ce010a630d4b5
CatalistaPOO <fgtpoo78@gmail.com>
2025-07-11 12:08:42 +0200

    Delete WS directory

--- WS/J02_introduccion2/src/introduccion02/HolaMundo.java
+++
@@ -1,8 +0,0 @@
-package introduccion02;
-
-public class HolaMundo {
-	public static void main (String[] arg) {
-		System.out.println("Viernes!!!");
-	}
-	// para ejecutar el archivo el atajo es: command + mundo + F11
-}
