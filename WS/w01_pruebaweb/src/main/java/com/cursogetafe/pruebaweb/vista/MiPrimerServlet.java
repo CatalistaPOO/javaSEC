b868335a748a89d48e1da4c2d95fbdde0451b299
CatalistaPOO <fgtpoo78@gmail.com>
2025-08-03 22:20:08 +0200

    SEC 3 agosto

---
+++ WS/w01_pruebaweb/src/main/java/com/cursogetafe/pruebaweb/vista/MiPrimerServlet.java
@@ -0,0 +1,75 @@
+package com.cursogetafe.pruebaweb.vista;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/saludo.html")//cuando accede a esta url usa estos metodos
+public class MiPrimerServlet extends HttpServlet {
+
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doDelete(req, resp);
+	}
+	
+	@Override
+	//este metodo recibe una petición y una respuesta 
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("Has accedido al Get()");
+		String respuesta = "<!DOCTYPE html> <html> <body> <h1>Holaa soy un Servlet</h1> </body> </html>";
+		
+		PrintWriter out = new PrintWriter(resp.getWriter());
+		out.println(respuesta);
+	}
+
+	@Override
+	protected void doHead(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doHead(arg0, arg1);
+	}
+
+	@Override
+	protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doOptions(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPut(req, resp);
+	}
+
+	@Override
+	protected void doTrace(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doTrace(arg0, arg1);
+	}
+
+	@Override
+	protected long getLastModified(HttpServletRequest req) {
+		// TODO Auto-generated method stub
+		return super.getLastModified(req);
+	}
+
+	@Override
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.service(arg0, arg1);
+	}
+	
+	
+	
+}
